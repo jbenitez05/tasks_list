60192f9ed6cca8f9e9b28586bfad15c12c226638
Jorge Benítez <jorge.benitez.1991@gmail.com>
2024-07-10 17:33:58 -0500

    Feature: login primitivo

--- models/db.py
+++ models/db.py
@@ -2,14 +2,15 @@
 
 from config import parameters
 from pydal import Field
-from pydal.validators import IS_EMAIL
+from pydal.validators import IS_EMAIL, IS_NOT_EMPTY
 import datetime
 
 db = parameters.db
 
 db.define_table('auth_user',
-                Field('first_name'),
-                Field('last_name'),
-                Field('email','string',requires=IS_EMAIL()),
-                Field('created_on','datetime',default=datetime.datetime.now()),
+                Field('first_name', 'string',   requires=IS_NOT_EMPTY()         ),
+                Field('last_name',  'string',   requires=IS_NOT_EMPTY()         ),
+                Field('email',      'string',   requires=IS_EMAIL()             ),
+                Field('password',   'string',   requires=IS_NOT_EMPTY()         ),
+                Field('created_on', 'datetime', default=datetime.datetime.now() ),
                 )
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, jsonify, render_template, url_for, redirect
+from flask import Flask, request, jsonify, render_template, url_for, redirect, flash, session
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+
 from models.db import db
 from config import parameters, nav_menu
 import datetime
@@ -12,48 +12,14 @@ app = Flask(__name__)
 
 # Configuración
 app.config['JWT_SECRET_KEY'] = parameters.secret_key
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(hours=1)
 jwt = JWTManager(app)
 
 @app.context_processor
 def inject_vars():
     year = datetime.datetime.now().year
-    menu = nav_menu.nav_routes
+    menu = nav_menu.nav_routes    
     return dict(year=year,menu=menu)
-
-@app.route('/register', methods=['POST'])
-def register():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-    
-    if not username or not password:
-        return jsonify({"msg": "Username and password required"}), 400
-    
-    #if User.query.filter_by(username=username).first():
-    #    return jsonify({"msg": "Username already exists"}), 400
-    
-    new_user = "" #User(username=username, password=generate_password_hash(password))
-    #db.session.add(new_user)
-    #db.session.commit()
-    
-    return jsonify({"msg": "User created successfully"}), 201
-
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-    
-    user = "" #User.query.filter_by(username=username).first()
-    if user and check_password_hash(user.password, password):
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token), 200
-    
-    return jsonify({"msg": "Bad username or password"}), 401
-
-@app.route('/protected', methods=['GET'])
-@jwt_required()
-def protected():
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
 
 @app.route('/')
 def index():
@@ -62,6 +28,55 @@ def index():
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    data = request.get_json()
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    password = data.get('password')
+    
+    if not first_name or not last_name or not email or not password :
+        return jsonify({"msg": "Faltan datos"}), 400
+
+    if db(db.auth_user.email == email).select().first():
+        return jsonify({"msg": "Ya existe un usuario con este correo"}), 400
+
+    hashed_password = generate_password_hash(password)
+    db.auth_user.validate_and_insert(
+        first_name=first_name,
+        last_name=last_name,
+        email=email, 
+        password=hashed_password
+        )
+    db.commit()
+    
+    return jsonify({"msg": "Usuario registrado exitosamente"}), 201
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = db(db.auth_user.email == email).select().first()
+    if user and check_password_hash(user.password, password):
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token), 200
+    
+    return jsonify({"msg": "Credenciales inválidas"}), 401
+
+@app.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    return jsonify({"msg": "Logout exitoso"}), 200
 
 @app.route('/about')
 def about():
